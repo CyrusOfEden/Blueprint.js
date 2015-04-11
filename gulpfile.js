9f5a195c9768d894a8b336810da46575d0e98129
Kash Nouroozi <knouroozi@gmail.com>
2015-04-11 17:36:20 -0400

    Fixed build settings.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var util = require('gulp-util');
-var source = require('vinyl-source-stream');
 
 gulp.task('default', function() {
   return gulp.src('./src/index.js')
